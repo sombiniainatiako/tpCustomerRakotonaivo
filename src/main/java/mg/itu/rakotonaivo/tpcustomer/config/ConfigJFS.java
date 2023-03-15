03a3df77233e131a352f3aaec0b463b2198fc8bb
RAKOTONAIVO RADO Tiako Sombiniaina <sombitiako@gmail.com>
2023-03-15 23:37:59 +0300

    Configuration pour JSF

---
+++ src/main/java/mg/itu/rakotonaivo/tpcustomer/config/ConfigJFS.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rakotonaivo.tpcustomer.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJFS {
+    
+}
