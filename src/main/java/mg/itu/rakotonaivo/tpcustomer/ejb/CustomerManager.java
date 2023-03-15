d69fd51bf3bde6201c7618b5a653af8359735684
RAKOTONAIVO RADO Tiako Sombiniaina <sombitiako@gmail.com>
2023-03-15 22:56:53 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/rakotonaivo/tpcustomer/ejb/CustomerManager.java
@@ -0,0 +1,32 @@
+
+package mg.itu.rakotonaivo.tpcustomer.ejb;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mg.itu.rakotonaivo.tpcustomer.entities.Customer;
+
+/**
+ * Gère la persistence des customers
+ * @author Rakotonaivo
+ */
+@Stateless
+public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {  
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList(); 
+    }  
+        
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }  
+}
